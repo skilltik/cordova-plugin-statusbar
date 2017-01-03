a30563767fb92576ef4be0e176835f142e425ed0
skilltik <skilltik@hotmail.com>
2017-01-03 16:49:31 -0300

    Update StatusBar.java

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -114,8 +114,12 @@ public class StatusBar extends CordovaPlugin {
                     // use KitKat here to be aligned with "Fullscreen"  preference
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                         int uiOptions = window.getDecorView().getSystemUiVisibility()
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+		| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+		| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+		| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+		| View.SYSTEM_UI_FLAG_FULLSCREEN
+		| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
                         window.getDecorView().setSystemUiVisibility(uiOptions);
                     }
